2035cf62b99175dcef51ed0ddde1baaa5542caa2
kevin janvier <kevin.janvier5@gmail.com>
2020-05-06 16:48:23 +0300

    start login service

---
+++ blood-bank-login/src/main/java/com/kevinjanvier/bloodbanklogin/BloodBankLoginApplication.java
@@ -0,0 +1,13 @@
+package com.kevinjanvier.bloodbanklogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodBankLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodBankLoginApplication.class, args);
+	}
+
+}
---
+++ blood-bank-login/src/test/java/com/kevinjanvier/bloodbanklogin/BloodBankLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinjanvier.bloodbanklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
